f8ebd31e12ae640ac8bfa46e311d8bacaca4e3bc
Huan Wu <huanwu2@illinois.edu>
2016-07-14 14:20:43 -0500

    [152. Maximum Product Subarray][Compile Error]committed by LeetCode Extension

--- 152-Maximum-Product-Subarray/solution.java
+++ 152-Maximum-Product-Subarray/solution.java
@@ -3,10 +3,12 @@ public class Solution {
         int preMax = nums[0];
         int preMin = nums[0];
         int result = nums[0];
+        int currMax = nums[0];
+        int ucrrMin = nums[0];
         for ( int i = 1; i < nums.length; ++i ){
-            int currMax = Math.max ( Math.max(preMax * nums[i], preMin * nums[i]), nums[i] );
-            int currMin = Math.min ( Math.min(preMin * nums[i], preMin * nums[i]), nums[i] );
-            preMath = currMax;
+            currMax = Math.max ( Math.max(preMax * nums[i], preMin * nums[i]), nums[i] );
+            currMin = Math.min ( Math.min(preMin * nums[i], preMin * nums[i]), nums[i] );
+            preMax = currMax;
             preMin = currMin;
             result = Math.max ( preMax, result );
         }
