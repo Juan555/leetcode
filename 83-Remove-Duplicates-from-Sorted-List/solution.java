9aab52781ba88859d9dedd3e6a6a51fb16eac7d0
Huan Wu <huanwu2@illinois.edu>
2016-07-14 01:23:10 -0500

    [83. Remove Duplicates from Sorted List][Runtime Error]committed by LeetCode Extension

--- 83-Remove-Duplicates-from-Sorted-List/solution.java
+++ 83-Remove-Duplicates-from-Sorted-List/solution.java
@@ -9,7 +9,7 @@
 public class Solution {
     public ListNode deleteDuplicates(ListNode head) {
         ListNode current = head;
-        while ( current == null && current.next == null ) {
+        while ( current == null || current.next == null ) {
             if ( current.val == current.next.val ){
                 current.next = current.next.next;
             }
