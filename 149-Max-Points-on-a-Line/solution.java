cb6dc917a3c4886bf234787bc614ef2627b4b0f4
Huan Wu <huanwu2@illinois.edu>
2016-07-15 00:13:15 -0500

    [149. Max Points on a Line][Accepted]committed by LeetCode Extension

--- 149-Max-Points-on-a-Line/solution.java
+++ 149-Max-Points-on-a-Line/solution.java
@@ -9,62 +9,72 @@
  */
 public class Solution {
     public int maxPoints(Point[] points) {
-        if (points.length < 3) return points.length; // 0/1/2 points
-        int res = 1; // at least 1 point
-        Map<String, Integer> map = new HashMap<String, Integer>(); // line,count
-        for (int i = 0; i < points.length; i++) {
+        if ( points.length < 3 ){
+            return points.length;
+        }
+        int result = 1; //at least two points in a line
+        Map<String, Integer> map = new HashMap<String, Integer>();
+        for ( int i = 0; i < points.length; ++i ){
+            int same_num = 0;
             int max = 0;
-            int countSame = 0; // # of same points
-            for (int j = i + 1; j < points.length; j++) {
-                if (points[i].x == points[j].x && points[i].y == points[j].y ) countSame++; // same point
-                else {
-                    String key = get_key(points[i], points[j]); // a|b|c
-                    if (map.containsKey(key)) { // on the line
-                        int count = map.get(key) + 1;
-                        map.put(key, count); // update count
-                        if (count > max) max = count; // update max
-                    } else { // not on any line
-                        map.put(key, 1);
-                        if (max == 0) max++; // update max
+            for ( int j = i + 1; j <points.length; ++j ){
+                if ( points[i].x == points[j].x && points[j].y == points[i].y ){
+                    same_num++;
+                }
+                else{
+                String key = get_key( points[i], points[j] );
+                if ( map.containsKey(key) ) {
+                    int count = map.get(key) + 1;
+                    map.put(key, count);
+                    if (count > max){
+                        max = count;
                     }
                 }
+                else{
+                    map.put(key, 1);
+                    if ( max == 0 ){
+                        max++;
+                    }
+                }
+                }
             }
-            res = Math.max(res, max + countSame + 1); // +1 for the start point
-            map.clear(); // clear map for next point
+            result = Math.max (result, max + same_num + 1);
+            map.clear();
         }
-        return res;
+        return result;
     }
     
-    private static String get_key(Point p1, Point p2) { //ax + by = c
-        int a, b;
-        float c;
-        if (p1.x == p2.x) { // vertical
-            b = 0;
+    private static String get_key(Point first, Point second) { //ax + by = c
+        int a;
+        int b;
+        float c; //pay attention it's float
+        if ( first.x == second.x ){ // vertical
             a = 1;
-            c = p1.x;
-        } else if (p1.y == p2.y) { // horizontal
+            b = 0;
+            c = first.x;
+        }
+        else if ( first.y == second.y ) { // horizontal
             a = 0;
             b = 1;
-            c = p2.y;
-        } else { // ax + by = c
-            int dx = p1.x - p2.x;
-            int dy = p1.y - p2.y;
-            /*reduce to simplest*/
-            int gcd = find_gcd(Math.abs(dx), Math.abs(dy));
-            a = dy / gcd;
-            b = dx / gcd;
-            if (a * b < 0) { // force a to be negative
-                a = -1 * Math.abs(a);
-                b = Math.abs(b);
-            } else { // force both positive
-                a = Math.abs(a);
-                b =  Math.abs(b);
-            }
-            c = a * p1.x + b * p1.y; // c = ax + by
+            c = second.y;
         }
-        return a + "|" + b + "|" + c; // key format
+        else{
+            int dx = first.x - second.x;
+        int dy = first.y - second.y;
+        int gcd = find_gcd( Math.abs(dx), Math.abs(dy) );
+        a = dy / gcd;
+        b = dx / gcd;
+        if ( a * b < 0 ){
+            a = -1 * Math.abs(a);
+        }
+        else if ( a * b > 0 ){
+            a = Math.abs(a);
+        }
+        b = Math.abs(b);
+        c = a * first.x + b * first.y;
+        }
+        return a + "|" + b + "|" + c;
     }
-
     
     private static int find_gcd( int a, int b ) {
         if ( b == 0 ){
