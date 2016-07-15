ff6d72c5108a57b1c1d793d195402062c6c10994
Huan Wu <huanwu2@illinois.edu>
2016-07-15 00:26:16 -0500

    [151. Reverse Words in a String][Compile Error]committed by LeetCode Extension

--- 151-Reverse-Words-in-a-String/solution.java
+++ 151-Reverse-Words-in-a-String/solution.java
@@ -2,7 +2,7 @@ public class Solution {
     public String reverseWords(String s) {
         String word = "";
         String result = "";
-        for ( int i = 0; i < s.length; ++i ) {
+        for ( int i = 0; i < s.length(); ++i ) {
             if ( s[i] == " "){
                 result = word + " " + result;
             }
