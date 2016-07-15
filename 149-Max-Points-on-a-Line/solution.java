b3a5455f24e5a6098f5c5abf3bc047bb971a265f
Huan Wu <huanwu2@illinois.edu>
2016-07-14 23:43:18 -0500

    [149. Max Points on a Line][Accepted]committed by LeetCode Extension

--- 149-Max-Points-on-a-Line/solution.java
+++ 149-Max-Points-on-a-Line/solution.java
@@ -9,76 +9,73 @@
  */
 public class Solution {
     public int maxPoints(Point[] points) {
-        if ( points.length < 3 ){
-            return points.length;
-        }
-        int result = 2; //at least two points in a line
-        int max = 0;
-        Map<String, Integer> map = new HashMap<String, Integer>();
-        for ( int i = 0; i < points.length; ++i ){
-            int same_num = 0;
-            for ( int j = i + 1; j <points.length; ++j ){
-                if ( points[i].x == points[j].x && points[j].y == points[i].y ){
-                    same_num++;
-                }
-                else{
-                String key = get_key( points[i], points[j] );
-                if ( map.containsKey(key) ) {
-                    int count = map.get(key);
-                    count++;
-                    map.put(key, count);
-                    if (count > max){
-                        max = count;
+        if (points.length < 3) return points.length; // 0/1/2 points
+        int res = 1; // at least 1 point
+        Map<String, Integer> map = new HashMap<String, Integer>(); // line,count
+        for (int i = 0; i < points.length; i++) {
+            int max = 0;
+            int countSame = 0; // # of same points
+            for (int j = i + 1; j < points.length; j++) {
+                if (points[i].x == points[j].x && points[i].y == points[j].y ) countSame++; // same point
+                else {
+                    String key = normalize(points[i], points[j]); // a|b|c
+                    if (map.containsKey(key)) { // on the line
+                        int count = map.get(key) + 1;
+                        map.put(key, count); // update count
+                        if (count > max) max = count; // update max
+                    } else { // not on any line
+                        map.put(key, 1);
+                        if (max == 0) max++; // update max
                     }
-                }
-                else{
-                    map.put(key, 1);
-                    if ( max == 0 ){
-                        max = 1;
-                    }
-                }
                 }
             }
-            result = Math.max (result, max + same_num + 1);
-            map.clear();
+            res = Math.max(res, max + countSame + 1); // +1 for the start point
+            map.clear(); // clear map for next point
         }
-        return result;
+        return res;
     }
-    
-    private static String get_key(Point first, Point second) { //ax + by = c
-        int a;
-        int b;
-        float c; //pay attention it's float
-        if ( first.x == second.x ){ // vertical
-            a = 1;
+
+    /**
+     * use ax + by = c to represent a line and a|b|c as a key for that line
+     * a, b, c should be normalized, how?
+     *
+     * special case, vertical, horizontal
+     */
+    private static String normalize(Point p1, Point p2) {
+        int a, b;
+        float c;
+        if (p1.x == p2.x) { // vertical
             b = 0;
-            c = first.x;
-        }
-        if ( first.y == second.y ) { // horizontal
+            a = 1;
+            c = p1.x;
+        } else if (p1.y == p2.y) { // horizontal
             a = 0;
             b = 1;
-            c = first.y;
+            c = p2.y;
+        } else { // ax + by = c
+            int dx = p1.x - p2.x;
+            int dy = p1.y - p2.y;
+            /*reduce to simplest*/
+            int gcd = gcd(Math.abs(dx), Math.abs(dy));
+            a = dy / gcd;
+            b = dx / gcd;
+            if (a * b < 0) { // force a to be negative
+                a = -1 * Math.abs(a);
+                b = Math.abs(b);
+            } else { // force both positive
+                a = Math.abs(a);
+                b =  Math.abs(b);
+            }
+            c = a * p1.x + b * p1.y; // c = ax + by
         }
-        int dx = first.x - second.x;
-        int dy = first.y - second.y;
-        int gcd = find_gcd( Math.abs(dx), Math.abs(dy) );
-        a = dy / gcd;
-        b = dx / gcd;
-        if ( a * b < 0 ){
-            a = -Math.abs(a);
-        }
-        if ( a * b > 0 ){
-            a = Math.abs(a);
-        }
-        b = Math.abs(b);
-        c = a * first.x + b * first.y;
-        return "a" + "b" + "c";
+        return a + "|" + b + "|" + c; // key format
     }
-    
-    private static int find_gcd( int a, int b ) {
-        if ( b == 0 ){
-            return a;
-        }
-        return find_gcd ( b, b % a );
+
+    /**
+     * recursively calculate the greateset common divisor of two numbers
+     */
+    private static int gcd(int a, int b) {
+        if (b == 0) return a; // stop when b == 0, a is the gcd
+        return gcd(b, a % b); // a <- b, b <- a % b
     }
 }
