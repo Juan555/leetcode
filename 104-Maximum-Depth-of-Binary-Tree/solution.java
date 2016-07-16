67b34f9fe5fb170c080ae81c427e310672c9312f
Huan Wu <huanwu2@illinois.edu>
2016-07-16 00:55:33 -0500

    [104. Maximum Depth of Binary Tree][Compile Error]committed by LeetCode Extension

--- 104-Maximum-Depth-of-Binary-Tree/solution.java
+++ 104-Maximum-Depth-of-Binary-Tree/solution.java
@@ -18,8 +18,8 @@ public class Solution {
     
     public int max_helper(TreeNode curr, int a, int max) {
         if (root == null) return 0;
-        int left = maxDepth(root.left);
-        int right = maxDepth(root.right);
+        int left = maxDepth(root.left, a, max);
+        int right = maxDepth(root.right, a, max);
         return Math.max(left, right) + 1;
     }
 }
