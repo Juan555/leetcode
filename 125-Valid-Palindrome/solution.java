ee5d434fedb99c2db35100ce15c32ed879201d00
Huan Wu <huanwu2@illinois.edu>
2016-07-15 23:38:00 -0500

    [125. Valid Palindrome][Compile Error]committed by LeetCode Extension

--- 125-Valid-Palindrome/solution.java
+++ 125-Valid-Palindrome/solution.java
@@ -1,11 +1,16 @@
 public class Solution {
     public boolean isPalindrome(String s) {
-        StringBuilder sb = new StringBuilder();
-        for (char c : s.toCharArray()) if (Character.isLetterOrDigit(c)) sb.append(c);
-        String copy = sb.toString().toLowerCase();
-        int length = copy.length();
-        for (int i = 0; i < length / 2; i++) {
-            if (copy.charAt(i) != copy.charAt(length - (i + 1))) {
+        s = s.toLowerCase(); //to lower cases
+        StringBuilder result = new StringBuilder();
+        for ( int i = 0; i < s.length(); ++i ) {
+            if ( Character.isLetterOrDigit(s[i]) ) {
+                result.append(s[i]);
+            }
+        }
+        String copy = result.toString();
+        int array_length = copy.length();
+        for ( int i = 0; i < array_length/2; ++i ) {
+            if ( result[i].equals(result[array_length - 1 - i]) ){
                 return false;
             }
         }
