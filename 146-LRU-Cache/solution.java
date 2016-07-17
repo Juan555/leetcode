749cba24e2e2ed26fc5c58e64e871751c0d12f1f
Huan Wu <huanwu2@illinois.edu>
2016-07-16 22:27:50 -0500

    [146. LRU Cache][Runtime Error]committed by LeetCode Extension

--- 146-LRU-Cache/solution.java
+++ 146-LRU-Cache/solution.java
@@ -19,10 +19,6 @@ public class LRUCache {
         }
         ListNode(){} //ListNode constructor, has bug missing{}
         
-        private void delete() {
-            prev.next = next;
-            if (next != null) next.prev = prev;
-        }
     }
     
     public LRUCache(int capacity) {
@@ -58,42 +54,24 @@ public class LRUCache {
         return result;
     }
     
-    // public void set(int key, int value) {
-    //     if ( map.containsKey(key) ) {
-    //         ListNode curr_node = map.get(key);
-    //         curr_node.val = value;
-    //         move_to_end(curr_node);
-    //     }
-        
-    //     else {
-    //         if ( map.size() + 1 >= capa) { //find head, remove head and add to tail; find head in map, replace head
-    //             int temp_head = head.node_key;
-    //             map.remove(temp_head);
-    //             if (head != null ) {head = head.next;}//in ListNode
-                
-                
-    //         }
-    //         ListNode new_tail = new ListNode(key, value);
-    //         add_to_end(new_tail);
-    //         map.put(key, new_tail);
-    //         //add to map; add to tail in ListNode
-    //     }
-    // }
     public void set(int key, int value) {
-        if (map.containsKey(key)) {
-            ListNode n = map.get(key);
-            n.val = value;
-            move_to_end(n);
-            return;
+        if ( map.containsKey(key) ) {
+            ListNode curr_node = map.get(key);
+            curr_node.val = value;
+            move_to_end(curr_node);
         }
-        ListNode n = new ListNode(key, value); // add new node
-        map.put(key, n);
-        add_to_end(n); // update usage
-
-        if (map.size() > capa) {
-            n = head.next;
-            map.remove(n.node_key);
-            n.delete(); // delete dummy
+        
+        else {
+            if ( map.size() + 1 >= capa) { //find head, remove head and add to tail; find head in map, replace head
+                map.remove(head.node_key);
+                head = head.next;
+                
+                
+            }
+            ListNode new_tail = new ListNode(key, value);
+            add_to_end(new_tail);
+            map.put(key, new_tail);
+            //add to map; add to tail in ListNode
         }
     }
 }
