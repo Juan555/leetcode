720a3063bc0f2dfbb8491e1d914cb54e9b531b61
Huan Wu <huanwu2@illinois.edu>
2016-07-17 14:42:33 -0500

    [154. Find Minimum in Rotated Sorted Array II][Compile Error]committed by LeetCode Extension

--- 154-Find-Minimum-in-Rotated-Sorted-Array-II/solution.java
+++ 154-Find-Minimum-in-Rotated-Sorted-Array-II/solution.java
@@ -1,37 +1,51 @@
 public class Solution {
     public int findMin(int[] nums) {
-        if ( nums.length == 1 ) {
-            return nums[0];
-        }
-        return binary_search( 0, nums.length - 1, nums );
-    }
+    //     if ( nums.length == 1 ) {
+    //         return nums[0];
+    //     }
+    //     return binary_search( 0, nums.length - 1, nums );
+    // }
     
-    public int binary_search ( int left, int right, int[] nums ) {
-        if (left == right) {
-            return nums[left];
-        }
-        if ( left < right && nums[left] == nums[right] ) {
-            return binary_search ( left, right - 1, nums );
-        }
-        if (nums[left] < nums[right] ) {
-            return nums[left];
-        }
+    // public int binary_search ( int left, int right, int[] nums ) {
+    //     if (left == right) {
+    //         return nums[left];
+    //     }
+    //     if ( left < right && nums[left] == nums[right] ) {
+    //         return binary_search ( left + 1, right, nums );
+    //     }
+    //     if (nums[left] < nums[right] ) {
+    //         return nums[left];
+    //     }
         
-        int mid = ( left + right ) / 2;
-        if ( nums[left] == nums[left + 1] ) {
-            return binary_search ( left + 1, right, nums );
+    //     int mid = ( left + right ) / 2;
+    //     if ( nums[left] == nums[left + 1] ) {
+    //         return binary_search ( left + 1, right, nums );
+    //     }
+    //     if ( nums[right] == nums[right - 1] ) {
+    //         return binary_search ( left, right - 1, nums );
+    //     }
+    //     if ( mid >= 1 && nums[mid] < nums[left] ) {
+    //         return nums[mid];
+    //     }
+    //     else if ( nums[mid] >= nums[left] && nums[mid] >= nums[right] ) {
+    //         return binary_search ( mid + 1 , right, nums );
+    //     }
+    //     else {
+    //         return binary_search ( left, mid - 1, nums );
+    //     }
+    if (num == null || num.length == 0) return 0;
+        int l = 0;
+        int r = num.length - 1;
+        while (l <= r) {
+            int k = l;
+            while (k <= r && num[k] == num[r]) k++;
+            
+            l = k;
+            if (num[l] < num[r]) return num[l];
+            int mid = l + (r - l) / 2;
+            if (num[mid] >= num[l]) l = mid + 1;
+            else r = mid;
         }
-        if ( nums[right] == nums[right - 1] ) {
-            return binary_search ( left, right - 1, nums );
-        }
-        if ( mid >= 1 && nums[mid] < nums[left] ) {
-            return nums[mid];
-        }
-        else if ( nums[mid] >= nums[left] && nums[mid] >= nums[right] ) {
-            return binary_search ( mid , right, nums );
-        }
-        else {
-            return binary_search ( left, mid , nums );
-        }
+        return l;
     }
 }
