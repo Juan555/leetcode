99cec0203d9dac251f3a9bc2c2699366cb252b27
Huan Wu <huanwu2@illinois.edu>
2016-07-17 17:44:06 -0500

    [155. Min Stack][Runtime Error]committed by LeetCode Extension

--- 155-Min-Stack/solution.java
+++ 155-Min-Stack/solution.java
@@ -9,28 +9,18 @@ public class MinStack {
     
     public void push(int x) {
         minStack.push (x);
-        if ( !helper_stack. isEmpty() ) {
-            if ( helper_stack.peek() < x) {
-                helper_stack.pop();
-                helper_stack.push(x);
-            }
-        }
-        else {
+        if ( helper_stack. isEmpty() || helper_stack.peek() > x ) {
             helper_stack.push (x);
         }
-        
     }
     
     public void pop() {
         if ( !minStack.isEmpty() ) {
-            int temp = minStack.peek();
-            minStack.pop();
-            if ( !helper_stack.isEmpty() ) {
-                if ( temp == helper_stack.peek() ) {
-                    helper_stack.pop();
-                }
+            if ( minStack.pop() == helper_stack.peek() ) {
+                helper_stack.pop();
             }
         }
+        else {return;}
         
     }
     
