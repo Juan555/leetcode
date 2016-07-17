44271d1ca6225658da6aea8d9dbca96cdc7b40a6
Huan Wu <huanwu2@illinois.edu>
2016-07-17 10:57:48 -0500

    [154. Find Minimum in Rotated Sorted Array II][Compile Error]committed by LeetCode Extension

--- 154-Find-Minimum-in-Rotated-Sorted-Array-II/solution.java
+++ 154-Find-Minimum-in-Rotated-Sorted-Array-II/solution.java
@@ -1,9 +1,9 @@
 public class Solution {
     public int findMin(int[] nums) {
-        if ( nums.size() == 1 ) {
+        if ( nums.length == 1 ) {
             return nums[0];
         }
-        for (int i = 1; i < nums.size(); ) {
+        for (int i = 1; i < nums.length; ) {
             if (nums[i] >= nums[i - 1]) {
                 ++i;
             }
