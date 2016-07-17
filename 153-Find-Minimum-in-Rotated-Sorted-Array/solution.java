9625b6ea9fabbb4ab76933a186da16758d59250c
Huan Wu <huanwu2@illinois.edu>
2016-07-17 11:38:29 -0500

    [153. Find Minimum in Rotated Sorted Array][Accepted]committed by LeetCode Extension

---
+++ 153-Find-Minimum-in-Rotated-Sorted-Array/solution.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public int findMin(int[] nums) {
+if(nums.length == 1){
+            return nums[0];
+        }  
+        return binarySearch(0, nums.length - 1, nums);
+    }
+    
+    public static int binarySearch(int left, int right, int[] nums){
+        if(left >= right){
+            return nums[left];
+        }
+        int mid  = ( left + right ) / 2;
+        if(mid >= 1 && nums[mid] < nums[mid -1]){
+            return nums[(left+right)/2];
+        }
+        else if(nums[mid] >= nums[left] && nums[mid] >= nums[right]){
+            return binarySearch(mid + 1 , right, nums);
+        }
+        else
+            return binarySearch(left, mid - 1, nums);
+    }
+}
