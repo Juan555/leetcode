b3ae61c28f3ddda30c57384f62e92ecef55a69dd
Huan Wu <huanwu2@illinois.edu>
2016-07-17 17:49:30 -0500

    [155. Min Stack][Accepted]committed by LeetCode Extension

--- 155-Min-Stack/solution.java
+++ 155-Min-Stack/solution.java
@@ -3,33 +3,43 @@ public class MinStack {
 
     /** initialize your data structure here. */
     
-    private Stack<Integer> helper_stack = new Stack<>();
+    private Stack<Integer> s = new Stack<>();
+    /**
+     * Standard solution, two stacks.
+     * A minStack to store minimums.
+     */
     private Stack<Integer> minStack = new Stack<>();
-    
-    
+
+    /**
+     * Push x to stack.
+     * After that, check min stack.
+     * Push to min stack if min stack is empty or x is smaller than the top of min stack.
+     */
     public void push(int x) {
-        minStack.push (x);
-        if ( helper_stack. isEmpty() || helper_stack.peek() > x ) {
-            helper_stack.push (x);
+        s.push(x);
+        if (minStack.isEmpty() || x <= minStack.peek()) { // even smaller
+            minStack.push(x);
         }
     }
-    
+
+    /**
+     * Pop from s.
+     * Check if the value is current min.
+     * If it is, pop from min stack as well.
+     */
     public void pop() {
-        if ( !minStack.isEmpty() ) {
-            if ( minStack.pop() == helper_stack.peek() ) {
-                helper_stack.pop();
-            }
-        }
-        else {return;}
-        
+        if (s.pop().equals(minStack.peek())) minStack.pop();
     }
-    
+
     public int top() {
-        return minStack.peek();
+        return s.peek();
     }
-    
+
+    /**
+     * Peek min stack to get current minimum.
+     */
     public int getMin() {
-        return helper_stack.peek();
+        return minStack.peek();
     }
 }
 
