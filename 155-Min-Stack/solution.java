df11cb31881c0796a22c6957207ffedd7b60610d
Huan Wu <huanwu2@illinois.edu>
2016-07-17 17:18:08 -0500

    [155. Min Stack][Compile Error]committed by LeetCode Extension

--- 155-Min-Stack/solution.java
+++ 155-Min-Stack/solution.java
@@ -3,8 +3,8 @@ public class MinStack {
 
     /** initialize your data structure here. */
     
-    <stack> helper_stack;
-    <stack> minStack;
+    private Stack<Integer> helper_stack = new Stack<>;
+    private Stack<Integer> minStack = new Stack<>;
     
     
     public void push(int x) {
