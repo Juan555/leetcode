2c6eee93897832253ff88313423d4a063e34f26d
Huan Wu <huanwu2@illinois.edu>
2016-07-17 00:26:06 -0500

    [88. Merge Sorted Array][Compile Error]committed by LeetCode Extension

--- 88-Merge-Sorted-Array/solution.java
+++ 88-Merge-Sorted-Array/solution.java
@@ -1,5 +1,8 @@
 public class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
+        if (n == 0) {
+            return;
+        }
         while ( m > 0 && n > 0 ) {
             if ( nums1[m - 1] > nums2[n- 1] ) {
                 nums1[m + n - 1] = nums1[m - 1];
