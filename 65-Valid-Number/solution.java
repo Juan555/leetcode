e2eb9f37984648710d66cde6cbc8965b7f3ac6b4
Huan Wu <huanwu2@illinois.edu>
2016-07-18 17:39:58 -0500

    [65. Valid Number][Compile Error]committed by LeetCode Extension

--- 65-Valid-Number/solution.java
+++ 65-Valid-Number/solution.java
@@ -57,7 +57,7 @@ public class Solution {
                 
             //      }
             //      else { return false; } 
-            }
+            
             ++i;
         }
         return num;//??
