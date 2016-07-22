fac71451ff1b41663f0f4f64e6a91ffb5e4f6a47
Huan Wu <huanwu2@illinois.edu>
2016-07-21 21:33:00 -0500

    [38. Count and Say][Runtime Error]committed by LeetCode Extension

---
+++ 38-Count-and-Say/solution.java
@@ -0,0 +1,26 @@
+public class Solution {
+    public String countAndSay(int n) {
+        String result = "1";
+        int i = 1;
+        while ( i <= n ) {
+            result = count_helper ( result );
+        }
+        return result;
+    }
+    public String count_helper ( String input ) {
+        StringBuilder output = new StringBuilder();
+        int length = input.length();
+        int total_count = 0;
+        
+        while ( total_count <= length ) {
+            int count = 1;
+            while ( total_count + count < length  && input.charAt(total_count) == input.charAt(total_count + count) ) {
+                count++;
+            }
+            total_count += count;
+            output.append( Integer.toString(count) + Integer.toString(input.charAt(total_count)) );
+            
+        }
+        return output.toString();
+    }
+}
