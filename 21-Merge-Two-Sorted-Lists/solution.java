f0baa0e2d6c9525b3c311b29b0e827c94a2b83d5
Huan Wu <huanwu2@illinois.edu>
2016-07-22 21:41:47 -0500

    [21. Merge Two Sorted Lists][Compile Error]committed by LeetCode Extension

--- 21-Merge-Two-Sorted-Lists/solution.java
+++ 21-Merge-Two-Sorted-Lists/solution.java
@@ -8,6 +8,8 @@
  */
 public class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if ( l1 == null ) { return l2; }
+        if ( l2 == null ) { return l1; }
         if ( l1.val > l2.val ) {ListNode head = new ListNode( l2.val ); }
         else { ListNode head = new ListNode( l1.val ); }
         ListNode curr = head;
@@ -20,6 +22,7 @@ public class Solution {
                 curr.next = l1;
                 l1 = l1.next;
             }
+            curr = curr.next;
         }
         return head;
     }
