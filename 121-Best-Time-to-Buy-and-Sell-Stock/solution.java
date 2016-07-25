3c07b4d0487826b6f624b912db9b9883348e9210
Huan Wu <huanwu2@illinois.edu>
2016-07-24 22:44:49 -0500

    [121. Best Time to Buy and Sell Stock][Runtime Error]committed by LeetCode Extension

---
+++ 121-Best-Time-to-Buy-and-Sell-Stock/solution.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public int maxProfit(int[] prices) {
+        int[] max = new int[prices.length - 1];
+        max[max.length - 1] = prices[prices.length - 1];
+        for ( int i = max.length - 1; i > 0; ++i ) {
+            if ( prices[i] > max[i] ) {
+                max[i] = prices[i];
+            }
+            else { max[i] = max[i + 1];}
+        }
+        int result = 0;
+        for ( int i = 0; i < prices.length - 1; ++i ) {
+            int diff = max[i] - prices[i];
+            if ( diff >  result ) { 
+                result = diff;
+            }
+        }
+        return result;
+    }
+}
