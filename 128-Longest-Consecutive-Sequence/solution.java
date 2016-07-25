8561c1fcd232cee2da542256f3330a9accf5c85e
Huan Wu <huanwu2@illinois.edu>
2016-07-25 12:16:07 -0500

    [128. Longest Consecutive Sequence][Accepted]committed by LeetCode Extension

--- 128-Longest-Consecutive-Sequence/solution.java
+++ 128-Longest-Consecutive-Sequence/solution.java
@@ -1,17 +1,20 @@
-import java.util.Arrays
+import java.util.Arrays;
 public class Solution {
     public int longestConsecutive(int[] nums) {
         if ( nums == null ) { return 0; }
         if ( nums.length == 1 ) { return 1; }
         Arrays.sort( nums );
-        int count = 0;
-        int max = 0;
+        int count = 1;
+        int max = 1;
         for ( int i = 1; i < nums.length; ++i ) {
-            if (nums[i] == nums[i - 1]) {
+            if (nums[i] == (nums[i - 1]+1)) {
                 count++;
                 if (count > max) { max = count; }
             }
-            else { count = 0;}
+            else if ( nums[i] == nums[i - 1] ) {
+                if (count > max) { max = count; }
+            }
+            else { count = 1;}
         }
         return max;
     }
