94d0be0955346f846bbfdb8ff9344ac6a0a72858
Huan Wu <huanwu2@illinois.edu>
2016-07-26 14:47:07 -0500

    [73. Set Matrix Zeroes][Compile Error]committed by LeetCode Extension

--- 73-Set-Matrix-Zeroes/solution.java
+++ 73-Set-Matrix-Zeroes/solution.java
@@ -1,16 +1,17 @@
 public class Solution {
     public void setZeroes(int[][] matrix) {
-        int row = matrix.size();
-        int column = matrix[0].size();
+        int row = matrix.length;
+        int column = matrix[0].length;
         for ( int i = 0; i < row; ++i ) {
+            if (matrix[i][])
             for ( int j = 0; j < column; ++j ) {
                 if ( matrix[i][j] == 0 ) {
                     matrix[i][0] = matrix[0][j] = 0;
                 }
             }
         }
-        for ( int i = 0; i < row; ++i ){
-            for ( int j = 0; j < column; ++j ){
+        for ( int i = row - 1; i > 0; --i ){
+            for ( int j = column - 1; j > 0; --j ){
                 if ( matrix[i][0] == 0 || matrix[0][j] == 0 ) {
                     matrix[i][j] = 0;
                 }
