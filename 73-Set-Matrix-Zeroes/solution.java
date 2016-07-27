596aab5ede331dd6ff68729650a6e9a5ab744e04
Huan Wu <huanwu2@illinois.edu>
2016-07-26 22:03:13 -0500

    [73. Set Matrix Zeroes][Accepted]committed by LeetCode Extension

--- 73-Set-Matrix-Zeroes/solution.java
+++ 73-Set-Matrix-Zeroes/solution.java
@@ -1,20 +1,25 @@
 public class Solution {
     public void setZeroes(int[][] matrix) {
+        boolean first_column = false;
         int row = matrix.length;
         int column = matrix[0].length;
         for ( int i = 0; i < row; ++i ) {
-            for ( int j = 0; j < column; ++j ) {
+            if ( matrix[i][0] == 0 ) { first_column = true; }
+            for ( int j = 1; j < column; ++j ) {
                 if ( matrix[i][j] == 0 ) {
                     matrix[i][0] = matrix[0][j] = 0;
                 }
             }
         }
         for ( int i = row - 1; i >= 0; --i ){
-            for ( int j = column - 1; j >0 0; --j ){
+            for ( int j = column - 1; j >= 1; --j ){
                 if ( matrix[i][0] == 0 || matrix[0][j] == 0 ) {
                     matrix[i][j] = 0;
                 }
             }
+            if ( first_column == true) {
+                    matrix[i][0] = 0;
+                }
         }
     }
 }
