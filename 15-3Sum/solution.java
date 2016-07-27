2f0c1f0a63e88f1894bb39cad7789547afb1aefe
Huan Wu <huanwu2@illinois.edu>
2016-07-27 15:48:06 -0500

    [15. 3Sum][Accepted]committed by LeetCode Extension

--- 15-3Sum/solution.java
+++ 15-3Sum/solution.java
@@ -33,7 +33,7 @@ public class Solution {
                 int high = nums.length - 1;
                 while ( low < high ) {
                     if ( nums[low] + nums[high] + nums[i] == 0 ) {
-                        result.add(Arrays.asList(nums[i],nums[low], nums[high]);
+                        result.add(Arrays.asList(nums[i],nums[low], nums[high]) );
                         while ( low < high && nums[low] == nums[low + 1]) {low ++;}
                         while ( low < high && nums[high] == nums[high - 1]) { high --;}
                         low++;
