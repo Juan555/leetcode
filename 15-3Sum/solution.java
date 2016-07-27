0a913f19bff323371e824017d8b73e95f8390dc2
Huan Wu <huanwu2@illinois.edu>
2016-07-27 15:21:12 -0500

    [15. 3Sum][Accepted]committed by LeetCode Extension

--- 15-3Sum/solution.java
+++ 15-3Sum/solution.java
@@ -1,23 +1,28 @@
 public class Solution {
-    public List<List<Integer>> threeSum(int[] nums) {
-        Arrays.sort(nums);
-        int length = nums.length;
-        List<List<Integer>> result = new LinkedList<>();
-        for ( int i = 0; i < length - 2; ++i ) {
-            for ( int j = i + 1; j < length - 1; ++j ) {
-                for ( int k =  j + 1; k < length; ++k ) {
-                    if ( nums[k] == -( nums[i] + nums[j] ) ) {
-                        List element = Arrays.asList(nums[i], nums[j], nums[k]);
-                        if ( result.size() != 0 && result[result.size() - 1] == element ) {
-                            continue;
-                        }
-                        else {
-                            result.add( element);
-                        }
-                    }
+    public List<List<Integer>> threeSum(int[] num) {
+         Arrays.sort(num);
+    List<List<Integer>> res = new LinkedList<>(); 
+    for (int i = 0; i < num.length-2; i++) {
+        if (i == 0 || (i > 0 && num[i] != num[i-1])) {
+            int lo = i+1, hi = num.length-1, sum = 0 - num[i];
+            while (lo < hi) {
+                if (num[lo] + num[hi] == sum) {
+                    res.add(Arrays.asList(num[i], num[lo], num[hi]));
+                    while (lo < hi && num[lo] == num[lo+1]) lo++;
+                    while (lo < hi && num[hi] == num[hi-1]) hi--;
+                    lo++; hi--;
+                } else if (num[lo] + num[hi] < sum) {
+                    // improve: skip duplicates
+                    while (lo < hi && num[lo] == num[lo+1]) lo++;
+                    lo++;
+                } else {
+                    // improve: skip duplicates
+                    while (lo < hi && num[hi] == num[hi-1]) hi--;
+                    hi--;
                 }
             }
         }
-        return result;
+    }
+    return res;
     }
 }
