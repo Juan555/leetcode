d19b0d4fbd55cef6cb69171b7df185e574a13fca
Huan Wu <huanwu2@illinois.edu>
2016-07-27 15:46:58 -0500

    [15. 3Sum][Compile Error]committed by LeetCode Extension

--- 15-3Sum/solution.java
+++ 15-3Sum/solution.java
@@ -1,28 +1,51 @@
 public class Solution {
-    public List<List<Integer>> threeSum(int[] num) {
-         Arrays.sort(num);
-    List<List<Integer>> res = new ArrayList<List<Integer>>(); 
-    for (int i = 0; i < num.length-2; i++) {
-        if (i == 0 || (i > 0 && num[i] != num[i-1])) {
-            int lo = i+1, hi = num.length-1, sum = 0 - num[i];
-            while (lo < hi) {
-                if (num[lo] + num[hi] == sum) {
-                    res.add(Arrays.asList(num[i], num[lo], num[hi]));
-                    while (lo < hi && num[lo] == num[lo+1]) lo++;
-                    while (lo < hi && num[hi] == num[hi-1]) hi--;
-                    lo++; hi--;
-                } else if (num[lo] + num[hi] < sum) {
-                    // improve: skip duplicates
-                    while (lo < hi && num[lo] == num[lo+1]) lo++;
-                    lo++;
-                } else {
-                    // improve: skip duplicates
-                    while (lo < hi && num[hi] == num[hi-1]) hi--;
-                    hi--;
+    public List<List<Integer>> threeSum(int[] nums) {
+        // Arrays.sort(nums);
+        // int length = nums.length;
+        // List<List<Integer>> result = new LinkedList<>();
+        // for ( int i = 0; i < length - 2; ++i ) {
+        //     if ( i > 0 && nums[i] == nums[i - 1]){continue;}
+        //     for ( int j = i + 1; j < length - 1; ++j ) {
+        //         for ( int k =  j + 1; k < length; ++k ) {
+        //             if ( nums[k] == -( nums[i] + nums[j] ) ) {
+        //                 List element = Arrays.asList(nums[i], nums[j], nums[k]);
+        //                 if ( result.size() != 0 && result.get(result.size() - 1) == element ) {
+        //                     continue;
+        //                 }
+        //                 else {
+        //                     result.add( element);
+        //                 }
+        //             }
+        //         }
+        //     }
+        // }
+        // return result;
+        
+        //sort the array
+        //make the first element fixed, and traverse for second and third
+        //skip the duplicate first element
+        //skip duplicate second and third element
+        Arrays.sort(nums);
+        List<List<Integer>> result = ArrayList<List<Integer>>();
+        for (int i = 0; i < nums.length - 2; ++i ) {
+            if ( i == 0 || (i > 0 && nums[i] != nums[i - 1])){
+                int low = i + 1;
+                int high = nums.length - 1;
+                while ( low < high ) {
+                    if ( nums[low] + nums[high] + nums[i] == 0 ) {
+                        result.add(Arrays.asList(nums[i],nums[low], nums[high]);
+                        while ( low < high && nums[low] == nums[low + 1]) {low ++;}
+                        while ( low < high && nums[high] == nums[high - 1]) { high --;}
+                        low++;
+                        high--;
+                    }
+                    else if ( nums[high] + nums[low] + nums[i] > 0 ) {
+                        high--;
+                    }
+                    else { low++;}
                 }
             }
+            else{continue;}
         }
-    }
-    return res;
     }
 }
