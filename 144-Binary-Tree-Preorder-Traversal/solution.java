b99d9946c87b6b7c637c0f19833025d7ca0ed79a
Huan Wu <huanwu2@illinois.edu>
2016-07-28 17:44:56 -0500

    [144. Binary Tree Preorder Traversal][Compile Error]committed by LeetCode Extension

--- 144-Binary-Tree-Preorder-Traversal/solution.java
+++ 144-Binary-Tree-Preorder-Traversal/solution.java
@@ -11,15 +11,14 @@ public class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
         List<Integer> result = new ArrayList<Integer>();
         if ( root == null ) {return result;}
-        return helper( root, result );
+        return result;
     }
-    public List<Integer> helper( TreeNode root, List<Integer> result ){
+    public void helper( TreeNode root, List<Integer> result ){
         if ( root == null ) return 0;
         else {
             result.add(root.val);
-            int left = helper( root.left, result );
-            int right = helper( root.right, result );
-            return result;
+            helper( root.left, result );
+            helper( root.right, result );
         }
     }
 }
