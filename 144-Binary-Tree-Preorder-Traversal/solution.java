739c644ab063c31c5d9ea74b58752a1deaf0e498
Huan Wu <huanwu2@illinois.edu>
2016-07-28 18:48:15 -0500

    [144. Binary Tree Preorder Traversal][Runtime Error]committed by LeetCode Extension

--- 144-Binary-Tree-Preorder-Traversal/solution.java
+++ 144-Binary-Tree-Preorder-Traversal/solution.java
@@ -24,16 +24,16 @@ public class Solution {
     //     }
     
     Stack<TreeNode> stack = new Stack<>();
-        List<Integer> traversal = new ArrayList<>();
-        if(root!=null){
-            stack.push(root);
-            while(!stack.isEmpty()){
-                TreeNode curr = stack.pop();
-                traversal.add(curr.val);
-                if(curr.right!=null){ stack.push(curr.right); }
-                if(curr.left!=null){ stack.push(curr.left);  }
-            }
-        }
-        return traversal;
+    List<Integer> result = new ArrayList<Integer>();
+    if ( root == null ) { return result; }
+    stack.push ( root );
+    while ( !stack.isEmpty() ) {
+        TreeNode curr = stack.pop();
+        result.add ( curr.val );
+        if ( root.right != null ) { stack.push ( curr.right ); }
+        if ( root.left != null ) { stack.push ( curr.left );}
+        
+    }
+    return result;
     }
 }
