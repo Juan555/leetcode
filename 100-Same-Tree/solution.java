e40cefa553481d3f669f17fd0eb1ef21f99ca356
Huan Wu <huanwu2@illinois.edu>
2016-07-29 15:34:20 -0500

    [100. Same Tree][Accepted]committed by LeetCode Extension

--- 100-Same-Tree/solution.java
+++ 100-Same-Tree/solution.java
@@ -20,16 +20,14 @@ public class Solution {
                 TreeNode for_p = p_stack.pop();
                 TreeNode for_q = q_stack.pop();
                 if ( for_p.val != for_q.val ) { return false; }
-                if ( for_p.left != null && for_q.left != null) {
-                    p_stack.push(for_p.left);
-                    q_stack.push(for_q.left);
-                }
-                if ( for_q.right != null && for_p.right != null ) {
-                    p_stack.push(for_p.right);
-                    q_stack.push(for_q.right);
-                }
+                if ( for_p.left != null){p_stack.push(for_p.left);}
+                if (for_q.left != null) {q_stack.push(for_q.left);}
+                if ( q_stack.size() != p_stack.size() ) {return false;}
+                if ( for_q.right != null) {q_stack.push(for_q.right);}
+                if (for_p.right != null ) {p_stack.push(for_p.right);}
+                if ( q_stack.size() != p_stack.size() ) {return false;}
             }
-            if ( !p_stack.isEmpty() || !q_stack.isEmpty ) {return false;}
+            if ( !p_stack.isEmpty() || !q_stack.isEmpty() ) {return false;}
             return true;
         }
     }
