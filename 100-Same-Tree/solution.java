ae190e0770888e2d0692a68d87c066f0a14662e5
Huan Wu <huanwu2@illinois.edu>
2016-07-29 15:50:03 -0500

    [100. Same Tree][Accepted]committed by LeetCode Extension

--- 100-Same-Tree/solution.java
+++ 100-Same-Tree/solution.java
@@ -9,26 +9,32 @@
  */
 public class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
+        //1st iterative
+        // if ( p == null && q == null ) {return true;}
+        // else if ( p == null || q == null ) { return false;}
+        // else {
+        //     Stack<TreeNode> p_stack = new Stack<>();
+        //     Stack<TreeNode> q_stack = new Stack<>();
+        //     p_stack.push( p );
+        //     q_stack.push( q );
+        //     while ( !p_stack.isEmpty() && !q_stack.isEmpty() ) {
+        //         TreeNode for_p = p_stack.pop();
+        //         TreeNode for_q = q_stack.pop();
+        //         if ( for_p.val != for_q.val ) { return false; }
+        //         if ( for_p.left != null){p_stack.push(for_p.left);}
+        //         if (for_q.left != null) {q_stack.push(for_q.left);}
+        //         if ( q_stack.size() != p_stack.size() ) {return false;}
+        //         if ( for_q.right != null) {q_stack.push(for_q.right);}
+        //         if (for_p.right != null ) {p_stack.push(for_p.right);}
+        //         if ( q_stack.size() != p_stack.size() ) {return false;}
+        //     }
+        //     if ( !p_stack.isEmpty() || !q_stack.isEmpty() ) {return false;}
+        //     return true;
+        // }
+        
         if ( p == null && q == null ) {return true;}
-        else if ( p == null || q == null ) { return false;}
-        else {
-            Stack<TreeNode> p_stack = new Stack<>();
-            Stack<TreeNode> q_stack = new Stack<>();
-            p_stack.push( p );
-            q_stack.push( q );
-            while ( !p_stack.isEmpty() && !q_stack.isEmpty() ) {
-                TreeNode for_p = p_stack.pop();
-                TreeNode for_q = q_stack.pop();
-                if ( for_p.val != for_q.val ) { return false; }
-                if ( for_p.left != null){p_stack.push(for_p.left);}
-                if (for_q.left != null) {q_stack.push(for_q.left);}
-                if ( q_stack.size() != p_stack.size() ) {return false;}
-                if ( for_q.right != null) {q_stack.push(for_q.right);}
-                if (for_p.right != null ) {p_stack.push(for_p.right);}
-                if ( q_stack.size() != p_stack.size() ) {return false;}
-            }
-            if ( !p_stack.isEmpty() || !q_stack.isEmpty() ) {return false;}
-            return true;
-        }
+        if ( p == null || q == null ) {return false;}
+        if ( p.val == q.val ) { return isSameTree( p.left, q.left ) && isSameTree( p.right, q.right );}
+        else{ return false;}
     }
 }
