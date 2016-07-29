828a5e23b3b9e7226f3ed59e9cc03fc768dc8784
Huan Wu <huanwu2@illinois.edu>
2016-07-29 01:21:36 -0500

    [107. Binary Tree Level Order Traversal II][Compile Error]committed by LeetCode Extension

---
+++ 107-Binary-Tree-Level-Order-Traversal-II/solution.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        if ( root == null ) { return result; }
+        helper ( root, result, 0 );
+        return result;
+    }
+    public void helper( TreeNode root, List<List<Integer>> result, int height ) {
+        if ( root == null ) { return; }
+        if ( height >= result.size() ) {
+            result.add(0, new ArrayList<Integer>() );
+        }
+        result.get(height).add( root );
+        helper ( root, result, height + 1);
+        helper ( root, result, height + 1);
+    }
+}
