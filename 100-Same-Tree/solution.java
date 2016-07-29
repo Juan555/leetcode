a9b8298789f6d9f5e23c3bb80e65a7e8f99ca92d
Huan Wu <huanwu2@illinois.edu>
2016-07-29 15:13:46 -0500

    [100. Same Tree][Compile Error]committed by LeetCode Extension

--- 100-Same-Tree/solution.java
+++ 100-Same-Tree/solution.java
@@ -10,7 +10,7 @@
 public class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
         if ( p == null && q == null ) {return true;}
-        else if ( p = null || q == null ) { return false;}
+        else if ( p == null || q == null ) { return false;}
         else {
             Stack<Integer> p_stack = new Stack<>();
             Stack<Integer> q_stack = new Stack<>();
