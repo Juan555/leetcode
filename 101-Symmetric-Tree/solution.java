54fe862a449346884d0d718832e89d3c099c651a
Huan Wu <huanwu2@illinois.edu>
2016-07-29 16:40:48 -0500

    [101. Symmetric Tree][Compile Error]committed by LeetCode Extension

--- 101-Symmetric-Tree/solution.java
+++ 101-Symmetric-Tree/solution.java
@@ -9,32 +9,43 @@
  */
 public class Solution {
     public boolean isSymmetric(TreeNode root) {
-        List<List<Integer>> result = new ArrayList<List<Integer>>();
         if ( root == null ) { return true; }
-        helper( root, result, 0 );
-        for ( int i = 0; i < result.size(); ++i ) {
-            int length = result.get(i).size();
-            for ( int j = 0; j < length/2; ++j ) {
-                if ( result.get(i).get(j) != result.get(i).get(length - j -1) ){
-                    return false;
-                }
-            }
+        return helper ( root.left, root.right );}
+    public boolean helper ( TreeNode left, TreeNode right ) {
+        if ( left == null && right == null ) {return true;}
+        if ( left == null ^ right == null ) { return false;}
+        if ( left == right ) {
+            return helper ( left.left, right.right ) && helper ( left.right, right.left );
         }
-        return true;
     }
-    public void helper( TreeNode root, List<List<Integer>> result, int height ) {
-        if ( root == null ) { 
-            if ( height >= result.size() ) {
-                result.add( new ArrayList<Integer>() );
-            }
-            result.get(height).add(0); 
-            return;
-        }
-        if ( height >= result.size() ) {
-            result.add( new ArrayList<Integer>() );
-        }
-        result.get(height).add(root.val);
-        helper( root.left, result, height + 1);
-        helper( root.right,result, height + 1);
-    }
+        
+        //我知道这个方法很烂。。。但是反正过了哈哈哈
+    //     List<List<Integer>> result = new ArrayList<List<Integer>>();
+    //     if ( root == null ) { return true; }
+    //     helper( root, result, 0 );
+    //     for ( int i = 0; i < result.size(); ++i ) {
+    //         int length = result.get(i).size();
+    //         for ( int j = 0; j < length/2; ++j ) {
+    //             if ( result.get(i).get(j) != result.get(i).get(length - j -1) ){
+    //                 return false;
+    //             }
+    //         }
+    //     }
+    //     return true;
+    // }
+    // public void helper( TreeNode root, List<List<Integer>> result, int height ) {
+    //     if ( root == null ) { 
+    //         if ( height >= result.size() ) {
+    //             result.add( new ArrayList<Integer>() );
+    //         }
+    //         result.get(height).add(0); 
+    //         return;
+    //     }
+    //     if ( height >= result.size() ) {
+    //         result.add( new ArrayList<Integer>() );
+    //     }
+    //     result.get(height).add(root.val);
+    //     helper( root.left, result, height + 1);
+    //     helper( root.right,result, height + 1);
+    
 }
