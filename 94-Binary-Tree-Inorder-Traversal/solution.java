897186dc450d1f831f118fb5ab2c2c5f4cd94471
Huan Wu <huanwu2@illinois.edu>
2016-07-28 23:33:09 -0500

    [94. Binary Tree Inorder Traversal][Accepted]committed by LeetCode Extension

--- 94-Binary-Tree-Inorder-Traversal/solution.java
+++ 94-Binary-Tree-Inorder-Traversal/solution.java
@@ -10,15 +10,29 @@
 public class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
         //1st solution
-        List<Integer> result = new ArrayList<>();
-        if ( root == null ) { return result; }
-        helper ( root, result );
-        return result;
+    //     List<Integer> result = new ArrayList<>();
+    //     if ( root == null ) { return result; }
+    //     helper ( root, result );
+    //     return result;
+    // }
+    // public void helper ( TreeNode root, List<Integer> result ) {
+    //     if ( root == null ) {return;}
+    //     helper( root.left, result );
+    //     result.add( root.val );
+    //     helper( root.right, result );
+    List<Integer> result = new ArrayList<>();
+    if ( root == null ) { return result; }
+    Stack<TreeNode> stack = new Stack<>();
+    TreeNode curr = root;
+    while ( curr != null || !stack.isEmpty() ) {
+        while ( curr != null ) {
+            stack.push ( curr );
+            curr = curr.left;
+        }
+        curr = stack.pop();
+        result.add ( curr.val );
+        curr = curr.right;
     }
-    public void helper ( TreeNode root, List<Integer> result ) {
-        if ( root == null ) {return;}
-        helper( root.left, result );
-        result.add( root.val );
-        helper( root.right, result );
+    return result;
     }
 }
