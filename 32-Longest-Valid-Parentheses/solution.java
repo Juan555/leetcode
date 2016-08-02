77c9bc47abad3fb30734100156b2f9a326d84207
Huan Wu <huanwu2@illinois.edu>
2016-08-01 19:48:19 -0500

    [32. Longest Valid Parentheses][Compile Error]committed by LeetCode Extension

---
+++ 32-Longest-Valid-Parentheses/solution.java
@@ -0,0 +1,32 @@
+public class Solution {
+    public int longestValidParentheses(String s) {
+     Stack<Integer> stack = new Stack<>();
+     int length = s.length();
+     for ( int i = 0; i < length; ++i ) {
+         char element = s.charAt(i);
+         if ( element == '(' ) {
+             stack.push(i);
+         }
+         else {
+             if ( !stack.isEmpty() ) {
+                 if (s[stack.peek()] == '(') {
+                     stack.pop();
+                 }
+                 else { stack.push(i); }
+             }
+             else { stack.push(i); }
+         }
+     }
+     if ( stack.isEmpty() ) { return length; }
+     int start = 0;
+     int end = length;
+     int max = 0;
+     while ( !stack.isEmpty() ) {
+         start = stack.pop();
+         max = Math.max( max, end - start - 1);
+         end = start;
+     }
+     max = Math.max( max, end - 1);
+     return max;
+    }
+}
