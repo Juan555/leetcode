17459ad35fff9a67bdedd866a9d81838e0348170
Huan Wu <huanwu2@illinois.edu>
2016-08-01 23:34:08 -0500

    [150. Evaluate Reverse Polish Notation][Compile Error]committed by LeetCode Extension

---
+++ 150-Evaluate-Reverse-Polish-Notation/solution.java
@@ -0,0 +1,29 @@
+public class Solution {
+    public int evalRPN(String[] tokens) {
+        int result = 0;
+        Stack<Integer> stack = new Stack<>();
+        if ( tokens == null || !String.toInteger(tokens[0]).isDigit ) { return 0; }//???
+        for ( int i = 0; i < tokens.length; ++i ) {
+            String element = tokens[i];
+            if ( element.isNumetric() ) {
+                stack.push( element );
+            }
+            if ( element == "+" || element == "-" || element == "*" || element == "/" ) {
+                if ( !stack.isEmpty() ) {
+                    int first = stack.pop();
+                    if ( !stack.isEmpty() ) {
+                        int second = stack.pop();
+                    }
+                    else { return result; }
+                }
+                else { return result; }
+                if ( element == "+" ) { result = first + second;}
+                else if ( element == "-" ) {result = first - second;}
+                else if ( element == "*" ) {result = first * second; }
+                else { result = first / second; }
+                stack.push ( result );
+            }
+        }
+        return result;
+    }
+}
