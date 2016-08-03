0cfe963cbd2e115c50c6c18762f01c6193fde151
Huan Wu <huanwu2@illinois.edu>
2016-08-02 20:58:31 -0500

    [90. Subsets II][Compile Error]committed by LeetCode Extension

--- 90-Subsets-II/solution.java
+++ 90-Subsets-II/solution.java
@@ -3,7 +3,7 @@ public class Solution {
         List<List<Integer>> result = new ArrayList<List<Integer>>();
         int total_number = 1<<nums.length;
         for (int i = 0; i < total_number; ++i ) {
-            List<Integer> subset = new List<Integer>();
+            List<Integer> subset = new ArrayList<Integer>();
             for ( int j = 0; j < nums.length; ++j ) {
                 if (( i>>j ) & 1 == 1) { subset.add(nums[j]); }
             }
