320b2cfd23bc6338c8646d71c66ce3ecb5b75c77
Huan Wu <huanwu2@illinois.edu>
2016-08-02 21:01:04 -0500

    [90. Subsets II][Accepted]committed by LeetCode Extension

--- 90-Subsets-II/solution.java
+++ 90-Subsets-II/solution.java
@@ -1,11 +1,12 @@
 public class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
         List<List<Integer>> result = new ArrayList<List<Integer>>();
         int total_number = 1<<nums.length;
         for (int i = 0; i < total_number; ++i ) {
             List<Integer> subset = new ArrayList<Integer>();
             for ( int j = 0; j < nums.length; ++j ) {
-                if (1&( i>>j )  == 1) { subset.add(nums[j]); }
+                if ((1&( i>>j ))  == 1) { subset.add(nums[j]); }
             }
             if ( !result.contains(subset) ) { result.add(subset); }
         }
