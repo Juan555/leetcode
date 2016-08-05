4c954417c4f45a122f3c6275f6c299307a4cbee9
Huan Wu <huanwu2@illinois.edu>
2016-08-05 15:43:15 -0500

    [47. Permutations II][Compile Error]committed by LeetCode Extension

---
+++ 47-Permutations-II/solution.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> result = new Arraylist<List<Integer>>();
+        Arrays.sort(nums);
+        if ( nums == null || nums.length == 0 ) { return result; }
+        List<Integer> template = new ArrayList<Integer>();
+        back_tracking ( result, nums, template, 0 );
+        return result;
+    }
+    public void back_tracking ( List<List<Integer>> result, int[] nums, List<Integer> template, int index ){
+        if ( template.size() == nums.length && !result.contains(template) ) {
+            result.add( template );
+            return;
+        }
+        for ( int i = 0; i <= template.size(); ++i ) {
+            List<Integer> copy = new ArrayList<Integer>(template);
+            template.add(i, nums[index]);
+            back_tracking( result, nums, copy, index + 1);
+        }
+    }
+}
