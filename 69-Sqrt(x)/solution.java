7bb19e6d07d3d9351798ab41889953828bdf53a1
Huan Wu <huanwu2@illinois.edu>
2016-08-06 15:26:55 -0500

    [69. Sqrt(x)][Time Limit Exceeded]committed by LeetCode Extension

--- 69-Sqrt(x)/solution.java
+++ 69-Sqrt(x)/solution.java
@@ -5,7 +5,7 @@ public class Solution {
         }
     int left = 1, right = x;
     while(left < right) {
-        int mid = left + (right - left) / 2;
+        int mid = (left + right) / 2;
         if(mid <= x / mid) {
             left = mid;
         } else {
