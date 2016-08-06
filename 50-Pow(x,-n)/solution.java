44bdd98c4fae9394b75eb2749724595faf01025c
Huan Wu <huanwu2@illinois.edu>
2016-08-06 11:58:00 -0500

    [50. Pow(x, n)][Time Limit Exceeded]committed by LeetCode Extension

--- 50-Pow(x,-n)/solution.java
+++ 50-Pow(x,-n)/solution.java
@@ -1,14 +1,24 @@
 public class Solution {
     public double myPow(double x, int n) { // Time Limit Exceeded 
-        if ( n < 0 ) {
-            x = 1/x;
-            n =  Math.abs(n);
-        }
-        return power( x, n );
+    //     if ( n < 0 ) {
+    //         return 1.0/power ( x, -n);
+    //     }
+    //     return power( x, n );
+    // }
+    // public double power ( double x, int n ) {
+    //     if ( n == 0 ) {return 1;}
+    //     double v = power ( x, n/2 );
+    //     return (n%2==1? (v * v * x) : (v * v));
+    
+    if ( n == 0 ) { return 1;}
+    if ( n < 0 ) {
+        x = 1.0/x;
+        n = -n;
     }
-    public double power ( double x, int n ) {
-        if ( n == 0 ) return 1;
-        double v = power ( x, n/2 );
-        return (n%2=1? (v * v * x) : (v * v));
+    double result = x;
+    for ( int i = 2; i <= n; ++i ) {
+        result *= x;
+    }
+    return result;
     }
 }
