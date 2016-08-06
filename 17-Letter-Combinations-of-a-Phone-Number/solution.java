c31f2204cb9928094b36f95ad2ea6e1dfe46f020
Huan Wu <huanwu2@illinois.edu>
2016-08-06 10:53:34 -0500

    [17. Letter Combinations of a Phone Number][Compile Error]committed by LeetCode Extension

--- 17-Letter-Combinations-of-a-Phone-Number/solution.java
+++ 17-Letter-Combinations-of-a-Phone-Number/solution.java
@@ -11,8 +11,10 @@ public class Solution {
         map.put('8', "tuv");
         map.put('9', "wxyz");
         List<String> result = new ArrayList<>();
+        if ( digits == null || digits.length() ==0 ) {return result;}
         String element = new String();
         back_tracking( result, digits, element, map, 0 );
+        return result;
     }
     public void back_tracking( List<String> result, String digits, String element, HashMap map, int index ) {
         if ( element.length() == digits.length() ) {
@@ -20,11 +22,11 @@ public class Solution {
             return;
         }
         for ( int i = 0; i < digits.length(); ++i ) {
-            char c = digits.charAt(i);
+            char c = digits.charAt(index);
             String s = (String)map.get(c);
-            element += s;
+            element += s.charAt(i);
             back_tracking( result, digits, element, map, index + 1);  
-          
+            element = element.substring(0, string.length() - 1 );
         }
     }
 }
