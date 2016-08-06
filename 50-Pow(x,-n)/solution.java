95efdbf1af70fdabd7e62e00e5b49bfca7b5aa2f
Huan Wu <huanwu2@illinois.edu>
2016-08-06 13:01:48 -0500

    [50. Pow(x, n)][Accepted]committed by LeetCode Extension

--- 50-Pow(x,-n)/solution.java
+++ 50-Pow(x,-n)/solution.java
@@ -1,24 +1,28 @@
 public class Solution {
     public double myPow(double x, int n) { // Time Limit Exceeded 
-    //     if ( n < 0 ) {
-    //         return 1.0/power ( x, -n);
-    //     }
-    //     return power( x, n );
-    // }
-    // public double power ( double x, int n ) {
-    //     if ( n == 0 ) {return 1;}
-    //     double v = power ( x, n/2 );
-    //     return (n%2==1? (v * v * x) : (v * v));
+        if(n == 0)
+            return 1;
+        
+        if(n == 1)
+            return x;
+        
+        if(n == -1)
+            return 1/x;
+
+        double num = myPow(x, n/2);       
+        return num * num * myPow(x, n%2); 
     
-    if ( n == 0 ) { return 1;}
-    if ( n < 0 ) {
-        x = 1.0/x;
-        n = -n;
-    }
-    double result = 1.0;
-    for ( int i = 1; i <= n; ++i ) {
-        result *= x;
-    }
-    return result;
+    // if ( n == 0 ) { return 1;} Time limit
+    // if ( n < 0 ) {
+    //     x = 1.0/x;
+    //     n = -n;
+    // }
+    // double result = 1.0;
+    // for ( int i = 1; i <= n; ++i ) {
+    //     result *= x;
+    // }
+    // return result;
+    
+
     }
 }
