9ff48e9bd1e9d0d55b97683061cff48f1476d391
Huan Wu <huanwu2@illinois.edu>
2016-08-05 19:59:58 -0500

    [77. Combinations][Accepted]committed by LeetCode Extension

--- 77-Combinations/solution.java
+++ 77-Combinations/solution.java
@@ -3,18 +3,18 @@ public class Solution {
         List<List<Integer>> result = new ArrayList<List<Integer>>();
         if ( k <= 0 || k > n || n <= 0 ) { return result; }
         List<Integer> template = new ArrayList<Integer>();
-        back_tracking( result, n, k, template, 1 )
+        back_tracking( result, n, k, template, 1 );
         return result;
     }
     
     public void back_tracking(List<List<Integer>> result, int n, int k, List<Integer> template, int index) {
         if (template.size() == k ) {
-            result.add(template);
+            result.add(new ArrayList<Integer>(template));
             return;
         }
-        for ( int i = index; i < n; ++i ) {
+        for ( int i = index; i <= n; ++i ) {
             template.add(i);
-            back_tracking(result, n, k, template, i);
+            back_tracking(result, n, k, template, i+1);
             template.remove(template.size() - 1 );
         }
     }
