695b07129d5440bce25c92827051318d48461878
Huan Wu <huanwu2@illinois.edu>
2016-08-06 17:38:23 -0500

    [55. Jump Game][Time Limit Exceeded]committed by LeetCode Extension

--- 55-Jump-Game/solution.java
+++ 55-Jump-Game/solution.java
@@ -1,9 +1,12 @@
 public class Solution {
     public boolean canJump(int[] nums) {
+        if ( nums.length ==1 ) {return true;}
         for ( int i = 0; i < nums.length; ) {
-            if ( i + nums[i] >= nums.length) { return false;}
-            else { i = i + nums[i]; }
+            if ( i + nums[i] == (nums.length-1) ) {return true;}
+            else if ( i + nums[i] >=nums.length) { return true;}
+            else if (nums[i] == 0) {return false;}
+            else { i = i + nums[i]-1; }
         }
-        return true;
+        return false;
     }
 }
