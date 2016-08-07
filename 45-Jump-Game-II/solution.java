bae82969af7d1b6734357657529ea53589a57085
Huan Wu <huanwu2@illinois.edu>
2016-08-07 14:30:27 -0500

    [45. Jump Game II][Accepted]committed by LeetCode Extension

--- 45-Jump-Game-II/solution.java
+++ 45-Jump-Game-II/solution.java
@@ -1,17 +1,15 @@
 public class Solution {
     public int jump(int[] nums) {
-        int max_index = 0;
+        int last_index = 0;
         int step = 0;
         int curr = 0;
         if (nums.length==1) {return 0;}
         for ( int i = 0; i < nums.length-1; ++i ) {
-            
-            if ( i >= max_index || curr < max_index) {
-                step++;
-            }
-            int curr = max_index;
-            max_index = Math.max( max_index, i + nums[i]);
-           
+           curr = Math.max( curr, i + nums[i] );
+           if (i == last_index) {
+               step++;
+               last_index = curr;
+           }
         }
         return step;
     }
