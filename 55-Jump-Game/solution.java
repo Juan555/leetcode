842b438bff462efe826a5da9bd316033e9b3a995
Huan Wu <huanwu2@illinois.edu>
2016-08-07 14:58:42 -0500

    [55. Jump Game][Accepted]committed by LeetCode Extension

--- 55-Jump-Game/solution.java
+++ 55-Jump-Game/solution.java
@@ -44,9 +44,11 @@ public class Solution {
         
         
         int max = 0;
-        for ( int i = 0; i < nums.length; ++i ) {
-            if ( i > max ) {return false;}
+        if (nums.length == 1){return true;}
+        for ( int i = 0; i < nums.length-1; ++i ) {
+            
             max = Math.max( max, i + nums[i]);
+            if ( i >= max ) {return false;}
         }
         return true;
     }
