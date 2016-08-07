e3c4999cb800c6b5a1ce375bc5c83211d3096eb9
Huan Wu <huanwu2@illinois.edu>
2016-08-07 15:51:59 -0500

    [121. Best Time to Buy and Sell Stock][Time Limit Exceeded]committed by LeetCode Extension

--- 121-Best-Time-to-Buy-and-Sell-Stock/solution.java
+++ 121-Best-Time-to-Buy-and-Sell-Stock/solution.java
@@ -1,19 +1,26 @@
 public class Solution {
     public int maxProfit(int[] prices) {
-        if ( prices.length <= 1 ) {return 0;}
-        int[] max = new int[prices.length - 1];
-        max[max.length - 1] = prices[prices.length - 1];
-        for ( int i = max.length - 1; i > 0; --i ) {
-            if ( prices[i] > max[i] ) {
-                max[i-1] = prices[i];
-            }
-            else { max[i-1] = max[i];}
-        }
+        // if ( prices.length <= 1 ) {return 0;}
+        // int[] max = new int[prices.length - 1];
+        // max[max.length - 1] = prices[prices.length - 1];
+        // for ( int i = max.length - 1; i > 0; --i ) {
+        //     if ( prices[i] > max[i] ) {
+        //         max[i-1] = prices[i];
+        //     }
+        //     else { max[i-1] = max[i];}
+        // }
+        // int result = 0;
+        // for ( int i = 0; i < prices.length - 1; ++i ) {
+        //     int diff = max[i] - prices[i];
+        //     if ( diff >  result ) { 
+        //         result = diff;
+        //     }
+        // }
+        // return result;
         int result = 0;
-        for ( int i = 0; i < prices.length - 1; ++i ) {
-            int diff = max[i] - prices[i];
-            if ( diff >  result ) { 
-                result = diff;
+        for ( int i = 0; i < prices.length-1; ++i ) {
+            for ( int j = i; j < prices.length; ++j ) {
+                result = Math.max ( result, prices[j] - prices[i] );
             }
         }
         return result;
