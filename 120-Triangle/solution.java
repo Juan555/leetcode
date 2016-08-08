283687584aac17132a7ca8ec6f138a84a4cdbd4e
Huan Wu <huanwu2@illinois.edu>
2016-08-08 12:13:59 -0500

    [120. Triangle][Compile Error]committed by LeetCode Extension

--- 120-Triangle/solution.java
+++ 120-Triangle/solution.java
@@ -1,13 +1,13 @@
 public class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
-        int min = INT_MAX;
+        int min = MAX_VALUE;
         int max = 0;
         for ( int i = 0; i < triangle.size(); ++i ) {
             for ( int j = 0; j < triangle.get(i).size(); ++j ) {
                 min = Math.min( triangle.get(i).get(j), min ); 
             }
             max += min;
-            min = INT_MAX;
+            min = MAX_VALUE;
         }
         return max;
     }
