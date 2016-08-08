c43e43d87e059e7acf3f16e831b6430fd98b1ec6
Huan Wu <huanwu2@illinois.edu>
2016-08-08 15:14:15 -0500

    [53. Maximum Subarray][Compile Error]committed by LeetCode Extension

---
+++ 53-Maximum-Subarray/solution.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public int maxSubArray(int[] nums) {
+        if ( nums == null || nums.length == 0 ) {return 0;}
+        int total_sum = nums[0];
+        for ( int i = 0; i < nums.length; ++i ) {
+            int sum = nums[i];
+            int max = sum;
+            for ( int j = i+1; j < nums.length; ++j ) {
+                sum += nums[j];
+                max = Math.max(max, sum);
+            }
+            total_sum = Math.max(total_sum, max);
+        }
+        return total_sum
+    }
+}
