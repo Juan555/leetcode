adfab381764634f46415a31f4910488c743927ce
Huan Wu <huanwu2@illinois.edu>
2016-08-08 22:35:34 -0500

    [64. Minimum Path Sum][Compile Error]committed by LeetCode Extension

---
+++ 64-Minimum-Path-Sum/solution.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public int minPathSum(int[][] grid) {
+        if ( grid.length == 0 || grid[0].length == 0 ) {return 0;}
+        for ( int i = grid.length - 1; i >= 0; --i ) {
+            for ( int j = grid.length - 1; j >= 0; --j ){
+                if ( i == gird.length - 1 && j == grid[0].length - 1 ) {
+                    continue;
+                }
+                else if ( i == grid.length - 1 ) {
+                    grid[i][j] = grid[i][j + 1];
+                }
+                else if ( j == grid[0].length - 1 ) {
+                    grid[i][j] = grid[i + 1][j];
+                }
+                else {
+                    grid[i][j] = grid[i + 1][j + 1];
+                }
+            }
+        }
+        return grid[0][0];
+    }
+}
