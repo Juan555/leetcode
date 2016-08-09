cba950dc6262b84f16894de32115eaf66cb89fb6
Huan Wu <huanwu2@illinois.edu>
2016-08-08 21:06:00 -0500

    [132. Palindrome Partitioning II][Compile Error]committed by LeetCode Extension

--- 132-Palindrome-Partitioning-II/solution.java
+++ 132-Palindrome-Partitioning-II/solution.java
@@ -2,6 +2,7 @@ public class Solution {
     public int minCut(String s) {
         int length = s.length();
         if ( length == 0 ) { return 0; }
+        char[] c = s.toCharArray();
         boolean [][] judge = new boolean[length][length];
         for ( int i = 0; i < length; ++i ) {
             for ( int j = 0; j < length; ++j ) {
@@ -12,9 +13,9 @@ public class Solution {
         for ( int i: cut ) { cut[i] = length - i - 1;} //max cut for each cell
         for ( int i = length - 1; i >= 0; --i ) {
             for ( int j = i; j < length; ++j ) {
-                if ( s[i] == s[j] && (j - i < 2 || judge[i + 1][j - 1]) ){
+                if ( c[j] == c[i] && (j - i < 2 || judge[i + 1][j - 1]) ){
                     judge[i][j] = true;
-                    cut[i] = Math.min( cut[i], cut[i + 1] + 1);
+                    cut[i] = Math.min( cut[i], (cut[j + 1] + 1));
                 }
             }
         }
