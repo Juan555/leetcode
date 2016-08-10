3ece4db5bc7e07f62e38244431d0b67bc19b1fd6
Huan Wu <huanwu2@illinois.edu>
2016-08-10 01:11:22 -0500

    [221. Maximal Square][Accepted]committed by LeetCode Extension

--- 221-Maximal-Square/solution.java
+++ 221-Maximal-Square/solution.java
@@ -2,14 +2,15 @@ public class Solution {
     public int maximalSquare(char[][] matrix) {
         int result = 0;
         if ( matrix.length == 0 || matrix[0].length == 0) {return 0;}
-        for ( int i = 1; i < matrix.length; ++i ) {
-            for ( int j = 1; j < matrix[0].length; ++j ) {
-                if ( matrix[i][j] == '1' ) {
-                    matrix[i][j] = Math.min(Math.min ( matrix[i - 1][j], matrix[i][j - 1]), matrix[i - 1][j - 1] ) + 1;
-                    result = Math.max( result, matrix[i][j]);
+        int [][] map = new int[matrix.length + 1][matrix[0].length + 1];
+        for ( int i = 1; i <= matrix.length; ++i ) {
+            for ( int j = 1; j <= matrix[0].length; ++j ) {
+                if ( matrix[i-1][j-1] == '1' ) {
+                    map[i][j] = Math.min(Math.min ( map[i - 1][j], map[i][j - 1]), map[i - 1][j - 1] ) + 1;
+                    result = Math.max( result, map[i][j]);
                 }
             }
         }
-        return result;
+        return result*result;
     }
 }
