8da4d0daba2536df5aa8e14887b881818cd4bc0d
Huan Wu <huanwu2@illinois.edu>
2016-08-10 00:57:51 -0500

    [221. Maximal Square][Compile Error]committed by LeetCode Extension

--- 221-Maximal-Square/solution.java
+++ 221-Maximal-Square/solution.java
@@ -4,7 +4,7 @@ public class Solution {
         if ( matrix.length == 0 || matrix[0].length == 0) {return 0;}
         for ( int i = 1; i < matrix.length; ++i ) {
             for ( int j = 1; j < matrix[0].length; ++j ) {
-                if ( matrix[i][j] == 1 ) {
+                if ( matrix[i][j] == '1' ) {
                     matrix[i][j] = Math.min(Math.min ( matrix[i - 1][j], matrix[i][j - 1]), matrix[i - 1][j - 1] ) + 1;
                     result = Math.max( result, matrix[i][j]);
                 }
