30490d450fc3c37a538f91ece6c01c6717fab5f2
Huan Wu <huanwu2@illinois.edu>
2016-08-11 21:39:07 -0500

    [75. Sort Colors][Accepted]committed by LeetCode Extension

--- 75-Sort-Colors/solution.java
+++ 75-Sort-Colors/solution.java
@@ -2,7 +2,7 @@ public class Solution {
     public void sortColors(int[] nums) {
         int red = 0;
         int white = 0;
-        int blue = 0
+        int blue = 0;
         for ( int i = 0; i < nums.length; ++i ){
             if ( nums[i] == 0 ) {red ++;}
             else if ( nums[i] == 1 ) {white++;}
