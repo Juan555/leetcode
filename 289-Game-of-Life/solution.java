1f714650908f8a71949631b96b53cd3720fb1957
Huan Wu <huanwu2@illinois.edu>
2016-08-13 20:58:56 -0500

    [289. Game of Life][Compile Error]committed by LeetCode Extension

--- 289-Game-of-Life/solution.java
+++ 289-Game-of-Life/solution.java
@@ -40,7 +40,7 @@ public class Solution {
         }
         for ( int i = 0; i < board.length; ++i ) {
             for ( int j = 0; j < board[0].length; ++j ) {
-              board[i][j] >> 1;
+              board[i][j] >>= 1;
             }
         }
     }
