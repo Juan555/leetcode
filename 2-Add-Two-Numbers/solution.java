72a337c9b17652c53b1a6d954fa214a780908c1a
Huan Wu <huanwu2@illinois.edu>
2016-08-22 09:56:46 -0500

    [2. Add Two Numbers][Compile Error]committed by LeetCode Extension

--- 2-Add-Two-Numbers/solution.java
+++ 2-Add-Two-Numbers/solution.java
@@ -15,7 +15,7 @@ public class Solution {
         }
         else { ListNode result = new ListNode(l1.val + l2.val);}
         
-        while ( l1.val != null || l2.val != null ) {
+        while ( l1 != null || l2 != null ) {
             if ( l1 == null ) {
                 if ( l2.val + update > 9 ) {
                     result.insert(0);
@@ -24,6 +24,7 @@ public class Solution {
                     result.insert(new ListNode(l2.val+update));
                     update = 0;
                 }
+                l2 = l2.next;
             }
             else if ( l2 == null ){
                 if ( l1.val + update > 9 ) {
@@ -33,6 +34,7 @@ public class Solution {
                     result.insert(new ListNode( l1.val+update ));
                     update = 0;
                 }
+                l1 = l1.next;
             }
             else{
                 int sum = l1.val + l2.val + update;
@@ -44,9 +46,9 @@ public class Solution {
                     result.insert(new ListNode(sum));
                     update = 0;
                 }
+                l1 = l1.next;
+                l2 = l2.next;
             }
-            l1 = l1.next;
-            l2 = l2.next;
         }
         if ( update == 1 ) { result.insert(new ListNode(1)); }
         return result;
