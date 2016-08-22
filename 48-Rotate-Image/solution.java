1fe4e68ffd7d1234d8da6788dc8caa6decb78347
Huan Wu <huanwu2@illinois.edu>
2016-08-21 22:04:44 -0500

    [48. Rotate Image][Accepted]committed by LeetCode Extension

--- 48-Rotate-Image/solution.java
+++ 48-Rotate-Image/solution.java
@@ -9,7 +9,7 @@ public class Solution {
         }
         for ( int i = 0; i < matrix.length; ++i ) {
             for ( int j = 0; j < matrix[0].length; ++j ) {
-                matrix[i][j] = matrix[j][matrix.length - i - 1];
+                matrix[i][j] = helper[matrix.length - j - 1][i];
             }
         } 
     }
