4a07f98f0f1c4fcf36b45fd782e1361cbf827c0c
Huan Wu <huanwu2@illinois.edu>
2016-08-24 15:38:16 -0500

    [8. String to Integer (atoi)][Compile Error]committed by LeetCode Extension

--- 8-String-to-Integer-(atoi)/solution.java
+++ 8-String-to-Integer-(atoi)/solution.java
@@ -6,20 +6,22 @@ public class Solution {
         
         //check sign
         int sign = 1;
+        int start = 0;
         if ( str.charAt(0) == '+' || str.charAt(0) == '-' ) {
             sign = str.charAt(0) == '+'?1:(-1);
+            start++;
         }
         //check digit and overflow
         int result = 0;
-        for ( int i = 0; i < str.length(); ++i ) {
+        for ( int i = start; i < str.length(); ++i ) {
             //skip the first sign
-            if ( str.charAt(0) == '+' || str.charAt(0) == '-' ) {continue;}
+            // if ( str.charAt(0) == '+' || str.charAt(0) == '-' ) {continue;}
             //check if it's valid digit
-            int digit = '9' - str.charAt(i);
+            int digit = str.charAt(i) - '0';
             if (( digit > 9 ) || ( digit < 0 )) { break; }
-            if ( INT_MAX - digit < result ) {return INT_MAX;}
-            else if ( INT_MIN + digit > result ) { return INT_MIN;}
-            else{ result = result*10 + sign == 1?digit:digit*(-1);}
+            if ( sign == 1 && Integer.MAX_VALUE - digit <= result*10 ) {return Integer.MAX_VALUE;}
+            else if ( sign == -1 && Integer.MIN_VALUE + digit >= result*10 ) { return Integer.MIN_VALUE ;}
+            else{ result = result*10 + (sign == 1?digit:digit*sign);}
         }
         return result;
     }
