cfeeeb9b91ed20ea145789d2fbf53ca1a69dc8a7
Huan Wu <huanwu2@illinois.edu>
2016-08-23 22:38:19 -0500

    [138. Copy List with Random Pointer][Accepted]committed by LeetCode Extension

--- 138-Copy-List-with-Random-Pointer/solution.java
+++ 138-Copy-List-with-Random-Pointer/solution.java
@@ -43,46 +43,25 @@ public class Solution {
         }
         orig_curr.next = null;
         return copy_head.next;
-        // if ( head == null ) {
-        //     RandomListNode null_copy = null;
-        //     return null_copy;
-        // }
-        // RandomListNode curr = new RandomListNode(head.label);
-        // RandomListNode new_head = curr;
-        // RandomListNode orig_curr = head;
-        // while ( orig_curr.next != null ) {
-        //     curr.next = new RandomListNode(orig_curr.next.label);
-        //     orig_curr = orig_curr.next;
-        //     curr = curr.next;
-        // }
-        // curr.next = null;
-        // orig_curr = head;
-        // curr = new_head;
-        // if ( head.random == head ) {
-        //     new_head.random = new_head;
-        // }
-        // while( orig_curr != null ){
-        //     if ( orig_curr.random == null ) {
-        //         curr.random = null;
-        //     }
-        //     else{
-        //     RandomListNode old_temp = head;
-        //     RandomListNode new_temp = new_head;
-        //     while ( old_temp.next != null ) {
-        //         if ( orig_curr.random == old_temp ){
-        //             curr.random = new_temp;
-        //             break;
-        //         }
-        //         else{
-        //             old_temp = old_temp.next;
-        //             new_temp = new_temp.next;
-        //         }
-                
-        //     }
-        //     }
-        //     orig_curr = orig_curr.next;
-        //     curr = curr.next;
-        // }
-        // return new_head;
+//         public RandomListNode copyRandomList(RandomListNode head) {
+// 	//<original, clone>
+//     HashMap<RandomListNode, RandomListNode> maps = new HashMap<RandomListNode, RandomListNode>();
+//     RandomListNode cursor = head;
+//     while(null != cursor){
+//         maps.put(cursor, new RandomListNode(cursor.label));
+//         cursor = cursor.next; 
+//     }
+//     cursor = head;
+//     while(null != cursor){
+//     	maps.get(cursor).random = maps.get(cursor.random);
+//     	cursor = cursor.next;
+//     }
+//     cursor = head;
+//     while(null != cursor){
+//     	maps.get(cursor).next = maps.get(cursor.next);
+//         cursor = cursor.next;
+//     }
+//     return maps.get(head);
+// }
     }
 }
