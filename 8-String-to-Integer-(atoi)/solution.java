dc00b8842dfa0288ab29feb55584831786f542c2
Huan Wu <huanwu2@illinois.edu>
2016-08-24 15:48:07 -0500

    [8. String to Integer (atoi)][Compile Error]committed by LeetCode Extension

--- 8-String-to-Integer-(atoi)/solution.java
+++ 8-String-to-Integer-(atoi)/solution.java
@@ -12,13 +12,13 @@ public class Solution {
             start++;
         }
         //check digit and overflow
-        long result = 0;
+        long sum = 0;
         for ( int i = start; i < str.length(); ++i ) {
             if ( !Character.isDigit(str.charAt(i)) ) {break;}
             sum = sum*10+(str.charAt(i) - '0');
             if ( sign == 1 && sum > Integer.MAX_VALUE ) {return Integer.MAX_VALUE;}
             if ( sign == -1 && sum < Integer.MIN_VALUE ){ return Integer.MIN_VALUE;}
         }
-        return (int)result*sign;
+        return (int)sum*sign;
     }
 }
