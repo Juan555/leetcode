d1ff265407b4b3f15bce1a08890ad9e948c17885
Huan Wu <huanwu2@illinois.edu>
2016-08-24 15:48:44 -0500

    [8. String to Integer (atoi)][Accepted]committed by LeetCode Extension

--- 8-String-to-Integer-(atoi)/solution.java
+++ 8-String-to-Integer-(atoi)/solution.java
@@ -13,12 +13,21 @@ public class Solution {
         }
         //check digit and overflow
         long sum = 0;
-        for ( int i = start; i < str.length(); ++i ) {
-            if ( !Character.isDigit(str.charAt(i)) ) {break;}
-            sum = sum*10+(str.charAt(i) - '0');
-            if ( sign == 1 && sum > Integer.MAX_VALUE ) {return Integer.MAX_VALUE;}
-            if ( sign == -1 && sum < Integer.MIN_VALUE ){ return Integer.MIN_VALUE;}
-        }
+        // for ( int i = start; i < str.length(); ++i ) {
+        //     if ( !Character.isDigit(str.charAt(i)) ) {break;}
+        //     sum = sum*10+(str.charAt(i) - '0');
+        //     if ( sign == 1 && sum > Integer.MAX_VALUE ) {return Integer.MAX_VALUE;}
+        //     if ( sign == -1 && sum < Integer.MIN_VALUE ){ return Integer.MIN_VALUE;}
+        // }
+        for (int i = start; i < str.length(); i++) {
+		if (!Character.isDigit(str.charAt(i)))
+			return (int) sum * sign;
+		sum = sum * 10 + str.charAt(i) - '0';
+		if (sign == 1 && sum > Integer.MAX_VALUE)
+			return Integer.MAX_VALUE;
+		if (sign == -1 && (-1) * sum < Integer.MIN_VALUE)
+			return Integer.MIN_VALUE;
+	}
         return (int)sum*sign;
     }
 }
