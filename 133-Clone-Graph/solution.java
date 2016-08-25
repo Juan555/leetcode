54b0a71d5ad28f0d2876c3cfdbf9f57c4479b247
Huan Wu <huanwu2@illinois.edu>
2016-08-25 16:49:58 -0500

    [133. Clone Graph][Compile Error]committed by LeetCode Extension

---
+++ 133-Clone-Graph/solution.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for undirected graph.
+ * class UndirectedGraphNode {
+ *     int label;
+ *     List<UndirectedGraphNode> neighbors;
+ *     UndirectedGraphNode(int x) { label = x; neighbors = new ArrayList<UndirectedGraphNode>(); }
+ * };
+ */
+public class Solution {
+    public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
+        HashMap<Integer, List<UndirectedGraphNode> map = new HashMap<Integer, List<UndirectedGraphNode>();
+        if ( node == null ) { return result;}
+        UndirectedGraphNode result = new UndirectedGraphNode(node.label);
+        map.put(node.label, result.neighbors);
+        helper( node, result, map );
+        return result;
+    }
+    public void helper(UndirectedGraphNode node,UndirectedGraphNode last, HashMap<Integer, List<UndirectedGraphNode> map){
+        if ( node == null ) {return;}
+        if ( map.containsKey(node.label) ){
+            map.get(node.label).insert(last);
+            node = node.next;}
+        else { 
+            UndirectedGraphNode new_one = new UndirectedGraphNode(node.label);
+            new_one.neighbors.insert(last);
+            last.neighbors.insert(new_one);
+            map.put(node.label, new_one.neighbors);
+            helper( node.next, new_one, map);
+        }
+    }
+}
