ebf1c8b422983f8ed5e10c209992ba8a3be72546
Huan Wu <huanwu2@illinois.edu>
2016-08-25 18:34:34 -0500

    [133. Clone Graph][Compile Error]committed by LeetCode Extension

--- 133-Clone-Graph/solution.java
+++ 133-Clone-Graph/solution.java
@@ -7,25 +7,22 @@
  * };
  */
 public class Solution {
+    HashMap<Integer, UndirectedGraphNode> map = new HashMap<Integer, UndirectedGraphNode>();
     public UndirectedGraphNode cloneGraph(UndirectedGraphNode node) {
-        HashMap<Integer, List<UndirectedGraphNode>> map = new HashMap<Integer, List<UndirectedGraphNode>>();
-        if ( node == null ) { return null;}
+        if ( node == null ) { return null; }
         UndirectedGraphNode result = new UndirectedGraphNode(node.label);
-        map.put(node.label, result.neighbors);
-        helper( node, result, map );
-        return result;
-    }
-    public void helper(UndirectedGraphNode node,UndirectedGraphNode last, HashMap<Integer, List<UndirectedGraphNode>> map){
+        helper( node);
+        return map.get(node.label);
+        }
+    public void helper(UndirectedGraphNode node, UndirectedGraphNode result ) {
         if ( node == null ) {return;}
-        if ( map.containsKey(node.label) ){
-            map.get(node.label).add(last);
-            node = node.next;}
-        else { 
-            UndirectedGraphNode new_one = new UndirectedGraphNode(node.label);
-            new_one.neighbors.add(last);
-            last.neighbors.add(new_one);
-            map.put(node.label, new_one.neighbors);
-            helper( node.next, new_one, map);
+        if ( map.containsKey(node.label) ) {
+            map.get(node.label).add(map.get(node.label));
+        }
+        else{UndirectedGraphNode new_one = new UndirectedGraphNode(node.label);
+        map.put( node.label, new_one );}
+        for (UndirectedGraphNode curr: node.neighbors) {
+            helper(curr);
         }
     }
 }
