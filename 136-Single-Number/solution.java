a3cc670369571744ce3f339694a660283ed2ad4c
Huan Wu <huanwu2@illinois.edu>
2016-08-31 21:00:31 -0500

    [136. Single Number][Accepted]committed by LeetCode Extension

--- 136-Single-Number/solution.java
+++ 136-Single-Number/solution.java
@@ -1,14 +1,19 @@
 public class Solution {
     public int singleNumber(int[] nums) {
-        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        // HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
         
-        for ( Integer i: nums ) {
-            if ( !map.containsKey(i) ) { map.put(i, 1);}
-            else { map.put(i, 2);}
+        // for ( Integer i: nums ) {
+        //     if ( !map.containsKey(i) ) { map.put(i, 1);}
+        //     else { map.put(i, 2);}
+        // }
+        // for ( Integer i: nums ) {
+        //     if ( map.get(i) == 1 ) { return i;}
+        // }
+        // return 0;
+        int result = 0;
+        for ( int i = 0; i < nums.length; ++i ) {
+            result ^= nums[i];
         }
-        for ( Integer i: nums ) {
-            if ( map.get(i) == 1 ) { return i;}
-        }
-        return 0;
+        return result;
     }
 }
