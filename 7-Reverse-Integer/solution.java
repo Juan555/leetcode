8a390393e1468b024020c718197a453c2e8cbc87
Huan Wu <huanwu2@illinois.edu>
2016-08-31 21:22:17 -0500

    [7. Reverse Integer][Accepted]committed by LeetCode Extension

--- 7-Reverse-Integer/solution.java
+++ 7-Reverse-Integer/solution.java
@@ -4,8 +4,10 @@ public class Solution {
         int sign = x>0? 1 : (-1);
         x = x>0? x : (-x);
         while ( x > 0 ) {
-            result = result* 10 + x/10;
-            x = x%10;
+            if ( sign == 1 && ((Integer.MAX_VALUE - x%10)/10) < result ){return 0;}
+            if ( sign == -1 && (Integer.MIN_VALUE + x%10)/10 > result*(-1) ){return 0;}
+            result = result* 10 + x%10;
+            x = x/10;
         }
         return result*sign;
     }
