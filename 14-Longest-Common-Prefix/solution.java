2e03541470f4bcc0bb7daa76faa7bde554975de8
Huan Wu <huanwu2@illinois.edu>
2016-08-31 22:25:49 -0500

    [14. Longest Common Prefix][Accepted]committed by LeetCode Extension

--- 14-Longest-Common-Prefix/solution.java
+++ 14-Longest-Common-Prefix/solution.java
@@ -4,7 +4,7 @@ public class Solution {
         String prev = strs[0];
         for ( int i = 1; i < strs.length; ++i ) {
             while ( strs[i].indexOf(prev) != 0 ) {
-                prev = prev.subString(0, prev.length()-1);
+                prev = prev.substring(0, prev.length()-1);
             }
         }
         return prev;
