69cdc26343c1c84e20ed33b7eda8447cb6de83fe
Huan Wu <huanwu2@illinois.edu>
2016-09-01 22:40:47 -0500

    [9. Palindrome Number][Time Limit Exceeded]committed by LeetCode Extension

---
+++ 9-Palindrome-Number/solution.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public boolean isPalindrome(int x) {
+        if ( x < 0 ) {return false;}
+        int first= x;
+        int second = 0;
+        int end = x;
+        while( x > 0 ) {
+            second = second * 10 + first%10;
+            first /= 10;
+            end /= 100;
+        }
+        return (first == second)||(second/10 == first);
+    }
+}
